3888b9d826e549d411a076f6d8501b28d8b6ce70
Carl Samuelsson <csamuelsson94@gmail.com>
2018-10-28 02:49:52 +0100

    Added support for hints by the --hints command line flag

--- QuestionGenerator.py
+++ QuestionGenerator.py
@@ -1,4 +1,13 @@
 from random import randint
+import argparse
+
+# Declare parser for the command line flags
+def parseFlags():
+    parser = argparse.ArgumentParser(description='Training on convergence modes proofs with hints.')
+    parser.add_argument("-hints", "--hints", action="store_true", help="Show available hints")
+
+    return parser.parse_args()
+
 
 class ConvergenceMode:
     # One mode of convergence can have exactly one implication, i.e. another mode of convergence being true
@@ -14,6 +23,12 @@ class ConvergenceMode:
     def getChildConvergence(self):
         return self.childConvergence
 
+    def setClues(self, clueArray):
+        self.clueArray = clueArray
+
+    def getClues(self):
+        return self.clueArray
+
 
 # Define the different types of convergences:
 distribution = ConvergenceMode("in distribution")
@@ -21,15 +36,30 @@ prob = ConvergenceMode("in probability", distribution)
 almostSure = ConvergenceMode("almost surely", prob)
 rmean = ConvergenceMode("in R mean", prob)
 
+# Set the clues:
+rmean.setClues(["Use Markov's inequality to rewrite the definition of the probability convergence"])
+prob.setClues(["Use the definition for the CDF for x + epsilon and x - epsilon",
+    "Split the probability space to |X_n - X| > and <= epsilon"])
+# TODO: Write proper hints for every proof:
+almostSure.setClues(["TBD", "TBD", "TBD", "TBD"])
+
 convergenceArray = [distribution, prob, almostSure, rmean]
 
-def QuestionParser():
-    randomNumber = randint(0, 3) # only 4 modes of convergences to consider, NOT subject to change
-    randomConvergenceMode = convergenceArray[randomNumber]
-    implicationConvergence = randomConvergenceMode.getChildConvergence().getCleanText()
+def QuestionParser(parentConvergence):
+    implicationConvergence = parentConvergence.getChildConvergence().getCleanText()
 
     print("Show that if a sequence of random variables converges %s, then the sequence converges %s" 
-        % (randomConvergenceMode.getCleanText(), implicationConvergence))
+        % (parentConvergence.getCleanText(), implicationConvergence))
+
+    if args.hints:
+        clues = parentConvergence.getClues()
+        print("\n\nCLUES:")
+        i = 1
+        for clue in clues:
+            print(i, ': ', clue)
+            i += 1
 
 if __name__ == '__main__':
-    QuestionParser()
+    args = parseFlags()
+    parentConvergence = convergenceArray[randint(1,3)] # only 3 modes of convergences to consider (not the grandchild)
+    QuestionParser(parentConvergence)
